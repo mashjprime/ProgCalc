fafeea5ef2ec8798f5799efc87271370cd931db0
Mash J Prime <mashjprime@gmail.com>
2020-03-01 00:48:11 +0800

    Initial GUI

--- src/gui.java
+++ src/gui.java
@@ -2,12 +2,12 @@ import javax.swing.*;
 
 class gui {
     public static void menu() {
+        // Testing
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
         JButton button1 = new JButton("Button 1");
         frame.getContentPane().add(button1);
         frame.setVisible(true);
-
     }
 }
